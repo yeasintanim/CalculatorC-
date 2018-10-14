f2a6f7ae3d4bf2e31b2eaee4dbb23a17bd869563
Yeasin Tanim <you@example.com>
2018-10-14 06:32:14 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.javaCheckBox = new System.Windows.Forms.CheckBox();
+            this.cSharpCheckBox = new System.Windows.Forms.CheckBox();
+            this.pythonCheckBox = new System.Windows.Forms.CheckBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addListBoxButton = new System.Windows.Forms.Button();
+            this.clearListBoxButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(181, 65);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(164, 20);
+            this.firstNumberTextBox.TabIndex = 0;
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(181, 104);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(164, 20);
+            this.secondNumberTextBox.TabIndex = 1;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(181, 143);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(164, 20);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result:";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(73, 187);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(169, 187);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(75, 23);
+            this.subtractButton.TabIndex = 3;
+            this.subtractButton.Text = "Subtract";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(275, 187);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplyButton.TabIndex = 4;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(73, 223);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(75, 23);
+            this.divideButton.TabIndex = 5;
+            this.divideButton.Text = "Divide";
+            this.divideButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(169, 223);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(143, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Calculator";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bangladesh",
+            "India",
+            "Japan"});
+            this.comboBox1.Location = new System.Drawing.Point(419, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // javaCheckBox
+            // 
+            this.javaCheckBox.AutoSize = true;
+            this.javaCheckBox.Location = new System.Drawing.Point(419, 156);
+            this.javaCheckBox.Name = "javaCheckBox";
+            this.javaCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.javaCheckBox.TabIndex = 10;
+            this.javaCheckBox.Text = "Java";
+            this.javaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cSharpCheckBox
+            // 
+            this.cSharpCheckBox.AutoSize = true;
+            this.cSharpCheckBox.Location = new System.Drawing.Point(419, 191);
+            this.cSharpCheckBox.Name = "cSharpCheckBox";
+            this.cSharpCheckBox.Size = new System.Drawing.Size(40, 17);
+            this.cSharpCheckBox.TabIndex = 10;
+            this.cSharpCheckBox.Text = "C#";
+            this.cSharpCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // pythonCheckBox
+            // 
+            this.pythonCheckBox.AutoSize = true;
+            this.pythonCheckBox.Location = new System.Drawing.Point(419, 223);
+            this.pythonCheckBox.Name = "pythonCheckBox";
+            this.pythonCheckBox.Size = new System.Drawing.Size(59, 17);
+            this.pythonCheckBox.TabIndex = 10;
+            this.pythonCheckBox.Text = "Python";
+            this.pythonCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(419, 256);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 11;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(647, 61);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(203, 199);
+            this.nameListBox.TabIndex = 12;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(647, 268);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(109, 20);
+            this.nameTextBox.TabIndex = 13;
+            // 
+            // addListBoxButton
+            // 
+            this.addListBoxButton.Location = new System.Drawing.Point(775, 266);
+            this.addListBoxButton.Name = "addListBoxButton";
+            this.addListBoxButton.Size = new System.Drawing.Size(75, 23);
+            this.addListBoxButton.TabIndex = 14;
+            this.addListBoxButton.Text = "Add";
+            this.addListBoxButton.UseVisualStyleBackColor = true;
+            this.addListBoxButton.Click += new System.EventHandler(this.addListBoxButton_Click);
+            // 
+            // clearListBoxButton
+            // 
+            this.clearListBoxButton.Location = new System.Drawing.Point(775, 295);
+            this.clearListBoxButton.Name = "clearListBoxButton";
+            this.clearListBoxButton.Size = new System.Drawing.Size(75, 23);
+            this.clearListBoxButton.TabIndex = 15;
+            this.clearListBoxButton.Text = "Clear";
+            this.clearListBoxButton.UseVisualStyleBackColor = true;
+            this.clearListBoxButton.Click += new System.EventHandler(this.clearListBoxButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(73, 304);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(272, 150);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 560);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.clearListBoxButton);
+            this.Controls.Add(this.addListBoxButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.pythonCheckBox);
+            this.Controls.Add(this.cSharpCheckBox);
+            this.Controls.Add(this.javaCheckBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox javaCheckBox;
+        private System.Windows.Forms.CheckBox cSharpCheckBox;
+        private System.Windows.Forms.CheckBox pythonCheckBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button addListBoxButton;
+        private System.Windows.Forms.Button clearListBoxButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            double secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            double result = firstNumber + secondNumber;
+
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNumberTextBox.Clear();
+            secondNumberTextBox.Text = "";
+            resultTextBox.Text = String.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.Text);
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+            string checkedString = "";
+
+            if (javaCheckBox.Checked)
+            {
+                checkedString += "Java,";
+            }
+            if (cSharpCheckBox.Checked)
+            {
+                checkedString += "C#,";
+                
+            }
+            if (pythonCheckBox.Checked)
+            {
+                checkedString += "Python,";
+            }
+
+            checkedString = checkedString.Substring(0, checkedString.Length - 1);
+            MessageBox.Show(checkedString+" is checked");
+
+        }
+
+        private void addListBoxButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            nameListBox.Items.Add(name);
+        }
+
+        private void clearListBoxButton_Click(object sender, EventArgs e)
+        {
+            nameListBox.Items.Clear();
+        }
+    }
+}
